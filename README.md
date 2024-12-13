0e194b73c8c3cdf46aee8976dc1c0fad063ca14b
TsFreddie <whatis@tsdo.in>
2024-12-13 18:36:49 +0800

    release

---
+++ README.md
@@ -0,0 +1,29 @@
+# Blender Addon - Active Collection on Save
+
+This is a really simple addon to automatically change the active collection when saving a file. I made this for working with [Godot Engine](https://godotengine.org/).
+
+## Why do I need this?
+
+Godot is actually pretty good at importing `.blend` file directly. You can tag and skip things just by using a naming suffix. You should check out Godot's [Node type customization using name suffixes](https://docs.godotengine.org/en/4.3/tutorials/assets_pipeline/importing_3d_scenes/node_type_customization.html) doc before deciding on this addon.
+
+However, sometimes you run into other plugins that are hard to change their automatic naming behavior, or you just can't be bothered to change the name every time you create something like animation widgets or extra armatures for constrains. Here's a typical example for a blender setup:
+
+![Blender Setup](images/blender-outliner.png)
+
+Luckily, Godot has an `Active Collection Only` importer option, which is exactly what I needed, but modifying the model on the fly often changes the active collection. So I made this addon to automatically change the active collection when saving a file.
+
+## Godot Setup
+
+To turn on `Active Collection Only` import option, you need to bring up `Advanced Import Settings` by double clicking your `.blend` file in the `FileSystem` tab in the Godot editor.
+
+Then you can turn on the `Active Collection Only` option.
+
+![Active Collection Only](images/godot-importer.png)
+
+## Blender Setup
+
+After installing the addon, you can find the relative settings in the `Scene Properties` tab.
+
+![Scene Properties](images/blender-properties.png)
+
+Alternatively, you can simply right click a collection and select `Set as Saving Collection` to set it.
---
+++ __init__.py
@@ -0,0 +1,125 @@
+bl_info = {
+    "name": "Change Active Collection on Save",
+    "blender": (4, 3, 0),
+    "category": "Object",
+}
+
+import bpy
+
+def set_active_collection_by_pointer_recursive(target, layer_collection, context):
+    if layer_collection.collection == target:
+        context.view_layer.active_layer_collection = layer_collection
+        return True
+        
+    for child_layer_collection in layer_collection.children:
+        if set_active_collection_by_pointer_recursive(target, child_layer_collection, context):
+            return True
+    return False
+
+def set_active_collection_by_name_recursive(target, layer_collection, context):
+    if layer_collection.name == target:
+        context.view_layer.active_layer_collection = layer_collection
+        return True
+        
+    for child_layer_collection in layer_collection.children:
+        if set_active_collection_by_name_recursive(target, child_layer_collection, context):
+            return True
+    return False
+
+@bpy.app.handlers.persistent
+def on_save_pre(dummy):
+    context = bpy.context
+    context.scene.acos_previous_active_collection = context.view_layer.active_layer_collection.name
+
+    collection = context.scene.acos_saving_collection
+    root_layer_collection = context.view_layer.layer_collection
+    if collection:
+        set_active_collection_by_pointer_recursive(collection, root_layer_collection, context)
+
+@bpy.app.handlers.persistent
+def on_save_post(dummy):
+    context = bpy.context
+    collection_name = context.scene.acos_previous_active_collection
+    root_layer_collection = context.view_layer.layer_collection
+    if collection_name:
+        set_active_collection_by_name_recursive(collection_name, root_layer_collection, context)
+
+@bpy.app.handlers.persistent
+def load_post(dummy):
+    context = bpy.context
+    collection_name = context.scene.acos_previous_active_collection
+    root_layer_collection = context.view_layer.layer_collection
+    if collection_name:
+        set_active_collection_by_name_recursive(collection_name, root_layer_collection, context)
+
+class SetSavingCollection(bpy.types.Operator):
+    """Change Saving Collection"""
+    bl_idname = "object.set_acos_saving_collection"
+    bl_label = "Change Saving Collection"
+    
+    bl_options = {'REGISTER', 'UNDO'}
+    
+    target_name: bpy.props.StringProperty()
+
+    def execute(self, context):
+        collection = bpy.data.collections.get(self.target_name)
+        context.scene.acos_saving_collection = collection
+        return {'FINISHED'}
+
+def menu_func(self, context):
+    layout = self.layout
+    collection = context.collection
+    if collection:
+        is_acos_saving_collection = (collection == context.scene.acos_saving_collection)
+        icon = 'FILE_TICK' if not is_acos_saving_collection else 'FILE_HIDDEN'
+        text = "Set as Saving Collection" if not is_acos_saving_collection else "Unset as Saving Collection"
+
+        op = layout.operator(SetSavingCollection.bl_idname, text=text, icon=icon)
+        op.target_name = collection.name if not is_acos_saving_collection else ""
+
+class ChangeActiveCollectionPanel(bpy.types.Panel):
+    bl_label = "Active Collection on Save"
+    bl_idname = "SCENE_PT_active_collection"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = 'scene'
+
+    def draw(self, context):
+        layout = self.layout
+        layout.use_property_decorate = False
+        layout.use_property_split = True
+        layout.prop(context.scene, "acos_saving_collection")
+
+def register():
+    bpy.utils.register_class(SetSavingCollection)
+    bpy.utils.register_class(ChangeActiveCollectionPanel)
+    bpy.types.OUTLINER_MT_collection.append(menu_func)
+    
+    bpy.types.Scene.acos_saving_collection = bpy.props.PointerProperty(
+        name="Saving Collection",
+        type=bpy.types.Collection,
+        description="Collection to set as active on save"
+    )
+    bpy.types.Scene.acos_previous_active_collection = bpy.props.StringProperty(
+        name="Previous Active Collection",
+        description="The active collection before saving"
+    )
+    
+    bpy.app.handlers.save_pre.append(on_save_pre)
+    bpy.app.handlers.save_post.append(on_save_post)
+    bpy.app.handlers.load_post.append(load_post)
+
+def unregister():
+    bpy.utils.unregister_class(SetSavingCollection)
+    bpy.utils.unregister_class(ChangeActiveCollectionPanel)
+    bpy.types.OUTLINER_MT_collection.remove(menu_func)
+    
+    del bpy.types.Scene.acos_saving_collection
+    del bpy.types.Scene.acos_previous_active_collection
+    
+    bpy.app.handlers.save_pre.remove(on_save_pre)
+    bpy.app.handlers.save_post.remove(on_save_post)
+    bpy.app.handlers.load_post.remove(load_post)
+
+if __name__ == "__main__":
+    register()
